e89bb7e881c49c716a489427737b333f52c93412
Tatiana Smolin <tatismolin@gmail.com>
2019-09-18 23:57:59 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -2,7 +2,7 @@ def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
 	hash = {
-		name: "tanya"
+		railroads: "tanya"
 	}
 end
 hash
