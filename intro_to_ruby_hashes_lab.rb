92668952719de2b2bf6198e2c43c80d354064ccb
Tatiana Smolin <tatismolin@gmail.com>
2019-09-19 00:10:22 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,8 +25,9 @@ def monopoly_with_third_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
 	hash = {
-		rent_in_dollars: {
+		railroads: {
 			pieces: 4,
+			rent_in_dollars: {}
 		},
 	}
 end
