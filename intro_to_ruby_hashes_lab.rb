cba036bb4edfe3a698f1a5f6394d4cbdb1a08f23
Tatiana Smolin <tatismolin@gmail.com>
2019-09-19 00:01:14 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,8 +13,9 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
 	hash = {
-		railroads: {},
-		pieces: {}
+		railroads: {
+			pieces: {}
+		},
 	}
 end
 
